f40ffb78e0e008db18fa688c653e9106a6f91598
author <author@fourf>
2021-04-12 21:51:43 +0800

    fix: use date

--- src/services/github.ts
+++ src/services/github.ts
@@ -4,7 +4,7 @@ export async function getQDIIHis({code}:{code:string}){
     // const url = "https://raw.githubusercontent.com/wanghsinche/dourent/master/serverless/static/qdii.json?ts="+Date.now();
     const url = "https://cdn.jsdelivr.net/gh/wanghsinche/dourent@master/serverless/static/";
 
-    const res = await axios.get(`${url}${code}-data.json?ts=${Date.now()}`);
+    const res = await axios.get(`${url}${code}-data.json?ts=${new Date().getDate()}`);
     if (res.status === 200 && res.data){
         return res.data;
     }
